830a2e31df0c6bc266461da36ed0050abd4d77e9
kimjinmyeong <kjmdkdlel@gmail.com>
2024-02-20 23:37:39 +0900

    feat: add ProductController

---
+++ src/main/java/org/spring/study/data/controller/ProductController.java
@@ -0,0 +1,48 @@
+package org.spring.study.data.controller;
+
+import org.spring.study.data.dto.ChangeProductNameDto;
+import org.spring.study.data.dto.ProductDto;
+import org.spring.study.data.dto.ProductResponseDto;
+import org.spring.study.data.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ProductResponseDto> getProduct(Long number) {
+        ProductResponseDto productResponseDto = productService.getProduct(number);
+
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductDto productDto) {
+        ProductResponseDto productResponseDto = productService.saveProduct(productDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(productResponseDto);
+    }
+
+    @PutMapping
+    public ResponseEntity<ProductResponseDto> changeProductName(
+            @RequestBody ChangeProductNameDto changeProductNameDto) throws Exception {
+        ProductResponseDto productResponseDto = productService.changeProductName(changeProductNameDto.getNumber(), changeProductNameDto.getName());
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteProduct(Long number) throws Exception {
+        productService.deleteProduct(number);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
