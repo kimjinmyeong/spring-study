ec4096db57d364ec0f0cddc02a868ecf54f18c90
kimjinmyeong <kjmdkdlel@gmail.com>
2024-03-05 22:10:49 +0900

    feat: add entity fields

--- src/main/java/org/spring/study/data/entity/Product.java
+++ src/main/java/org/spring/study/data/entity/Product.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -27,5 +29,17 @@ public class Product extends BaseEntity {
 
     @Column(nullable = false)
     private Integer stock;
+
+    @OneToOne(mappedBy = "product")
+    private ProductDetail productDetail;
+
+    @ManyToOne
+    @JoinColumn(name = "provider_id")
+    @ToString.Exclude
+    private Provider provider;
+
+    @ManyToMany
+    @ToString.Exclude
+    private List<Producer> producers = new ArrayList<>();
 
 }
